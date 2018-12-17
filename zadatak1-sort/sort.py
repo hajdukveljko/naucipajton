9b7e5bed52aebf294f6d9435c670aa549502c33c
Hajduk Veljko <hajdukveljko@blahblah.com>
2018-12-16 22:23:44 -0800

    resenje sortiranja

--- zadatak1-sort/sort.py
+++ zadatak1-sort/sort.py
@@ -4,5 +4,16 @@ def ref_sort(in_list):
     return sorted(in_list)
 
 def my_sort(in_list):
-    # resi ovde
-    return None
+    if len(in_list) < 2:
+        return(in_list)
+    else:
+        for i in range(len(in_list)):
+            n = 1
+            while n < len(in_list):
+                if in_list[n] < in_list[n-1]:
+                    num1 = in_list[n]
+                    num2 = in_list[n-1]
+                    in_list[n] = num2
+                    in_list[n-1] = num1
+                n = n + 1
+    return in_list
