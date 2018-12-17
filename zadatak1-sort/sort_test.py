1f279e3f8f6b503a526ddc6883cf03806547cda8
Hajduk Veljko <hajdukveljko@blahblah.com>
2018-12-16 22:42:10 -0800

    Jos testova za sort

--- zadatak1-sort/sort_test.py
+++ zadatak1-sort/sort_test.py
@@ -1,5 +1,6 @@
 import unittest
 import sort
+import random
 
 
 class TestRefSort(unittest.TestCase):
@@ -35,6 +36,14 @@ class TestMySort(unittest.TestCase):
     def test_repeat(self):
         r = sort.my_sort([9, 4, 7, 2, 4, 0, 1, 0])
         self.assertEqual([0, 0, 1, 2, 4, 4, 7, 9], r)
+
+    def test_random(self):
+        for i in range(0, 100):
+            input_list = range(0, 100)
+            random.shuffle(input_list)
+            ref_sorted = sorted(input_list)
+            sorted_list = sort.my_sort(input_list)
+            self.assertEqual(ref_sorted, sorted_list)
 
 
 if __name__ == '__main__':
