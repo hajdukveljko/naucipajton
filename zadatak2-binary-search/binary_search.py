37b599e96f1e6a4fbb952fa7c93370e763bc8701
Hajduk Veljko <hajdukveljko@blahblah.com>
2018-12-16 22:57:19 -0800

    Zadatak 2 pripremljen

---
+++ zadatak2-binary-search/binary_search.py
@@ -0,0 +1,9 @@
+def ref_search(in_list, x):
+    for i in range(0, len(in_list)):
+        if in_list[i] == x:
+            return i
+    return -1
+
+
+def my_search(in_list, x):
+    return -1
---
+++ zadatak2-binary-search/binary_search_test.py
@@ -0,0 +1,61 @@
+import unittest
+import binary_search
+import random
+
+
+class TestRefSearch(unittest.TestCase):
+    def test_empty(self):
+        r = binary_search.ref_search([], 0)
+        self.assertEqual(-1, r)
+
+    def test_one(self):
+        r = binary_search.ref_search([1], 1)
+        self.assertEqual(0, r)
+
+        r = binary_search.ref_search([1], 5)
+        self.assertEqual(-1, r)
+
+
+    def test_few(self):
+        r = binary_search.ref_search([1, 5, 8, 11, 20, 21], 5)
+        self.assertEqual(1, r)
+
+        r = binary_search.ref_search([1, 5, 8, 11, 20, 21], 3)
+        self.assertEqual(-1, r)
+
+        r = binary_search.ref_search([1, 5, 8, 11, 20, 21], 1)
+        self.assertEqual(0, r)
+
+        r = binary_search.ref_search([1, 5, 8, 11, 20, 21], 20)
+        self.assertEqual(4, r)
+
+
+class TestMySearch(unittest.TestCase):
+    def test_empty(self):
+        r = binary_search.my_search([], 0)
+        self.assertEqual(-1, r)
+
+    def test_one(self):
+        r = binary_search.my_search([1], 1)
+        self.assertEqual(0, r)
+
+        r = binary_search.my_search([1], 5)
+        self.assertEqual(-1, r)
+
+
+    def test_few(self):
+        r = binary_search.my_search([1, 5, 8, 11, 20, 21], 5)
+        self.assertEqual(1, r)
+
+        r = binary_search.my_search([1, 5, 8, 11, 20, 21], 3)
+        self.assertEqual(-1, r)
+
+        r = binary_search.my_search([1, 5, 8, 11, 20, 21], 1)
+        self.assertEqual(0, r)
+
+        r = binary_search.my_search([1, 5, 8, 11, 20, 21], 20)
+        self.assertEqual(4, r)
+
+
+if __name__ == '__main__':
+    unittest.main()
