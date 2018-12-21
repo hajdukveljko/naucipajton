9c8cf8f376d1a90516c7e5bf8fdc30615c5e799e
Hajduk Veljko <hajdukveljko@blahblah.com>
2018-12-20 19:10:26 -0800

    nezavrsen zadatak2

--- zadatak2-binary-search/binary_search.py
+++ zadatak2-binary-search/binary_search.py
@@ -1,9 +1,29 @@
 def ref_search(in_list, x):
-    for i in range(0, len(in_list)):
-        if in_list[i] == x:
-            return i
-    return -1
+    if len(in_list) < 1 :
+        return -1
+    if x < in_list[0] or x > in_list[-1]:
+        return -1
+    if len(in_list)==1:
+        return 0
+    n = len(in_list)//2
+    indx1 = 0
+    indx2 = len(in_list)
+    d = (indx2-indx1)//2
+
+    while d >= 1:
+
+        if x == in_list[n]:
+            return n
+        elif x > in_list[n]:
+            indx1 = n
+            n = n + d
+        else:
+            indx2 = n
+            n = n - d
+        d = (indx2-indx1)//2
 
 
+
+print ref_search([22, 23],22)
 def my_search(in_list, x):
     return -1
